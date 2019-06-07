962fe44cb1572ccf5d2a21cc31faf179ac01ad59
AO Industries, Inc <support@aoindustries.com>
2019-06-06 23:54:45 -0500

    Using managed dependencies.
    This project uses transitive managed dependencies.
    This project's managed dependencies may also be imported by other projects.

--- src/main/java/com/pragmatickm/task/view/WhatToDoViewContextListener.java
+++ src/main/java/com/pragmatickm/task/view/WhatToDoViewContextListener.java
@@ -1,6 +1,6 @@
 /*
  * pragmatickm-task-view - SemanticCMS view of tasks in the current page and all children.
- * Copyright (C) 2016  AO Industries, Inc.
+ * Copyright (C) 2016, 2017  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
